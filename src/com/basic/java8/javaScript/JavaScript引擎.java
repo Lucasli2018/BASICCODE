96c24dda516c36d1ebeed5b1f9d734c9cd67b45c
Lucasli2018 <1906859953@qq.com>
2019-09-12 18:25:49 +0800

    java实现简单的计算器

---
+++ src/com/basic/java8/javaScript/JavaScript引擎.java
@@ -0,0 +1,18 @@
+package com.basic.java8.javaScript;
+
+/**
+ * Nashorn JavaScript
+ * 
+ * 与先前的Rhino实现相比，这带来了2到10倍的性能提升。
+ * 
+ * @author 50632
+ *
+ */
+public class JavaScript引擎 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
