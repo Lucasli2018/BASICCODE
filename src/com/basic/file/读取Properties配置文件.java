dd8f73268b8be2623f9dddee172a4a030d3e9409
lucas <1906859953@qq.com>
2019-09-16 23:37:23 +0800

    使用反射+配置文件 创建对象

--- src/com/basic/file/读取Properties配置文件.java
+++ src/com/basic/file/读取Properties配置文件.java
@@ -3,6 +3,7 @@ package com.basic.file;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
 import java.util.Properties;
@@ -14,12 +15,27 @@ public class 读取Properties配置文件 {
 	@Test
 	public void demo01() throws Exception{
 		File file=new File("D:\\github\\Javase-middle\\JKSHOP\\BASICCODE\\src\\com\\basic\\file\\test.properties");
+		String className = readProperties(file);
+		System.out.println("读取配置文件["+file.getAbsolutePath()+"]信息："+className);
+	}
+
+	public String readProperties(File file) throws FileNotFoundException, IOException {
 		InputStream inputStream=new FileInputStream(file);
 		//加载配置文件
 		Properties properties=new Properties();
 		properties.load(inputStream);
 		//根据key读取配置
 		String className=properties.getProperty("className");
-		System.out.println("读取配置文件["+file.getAbsolutePath()+"]信息："+className);
+		return className;
+	}
+	
+	@Test
+	public void demo02() throws Exception{
+		File file=new File("D:\\github\\Javase-middle\\JKSHOP\\BASICCODE\\src\\com\\basic\\file\\test.properties");
+		String className = readProperties(file);
+		
+		//使用反射+配置文件 创建对象
+		User newInstance = (User) Class.forName(className).newInstance();
+		newInstance.sayHello();
 	}
 }
